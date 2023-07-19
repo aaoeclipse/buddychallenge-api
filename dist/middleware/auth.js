90578e98f7545794120d2782d0d7fd39c6187326
aaoeclipse <santiago.paiz@gmail.com>
2023-07-19 15:13:20 +0200

    fix: :fire: remove distribution folder

--- dist/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.auth = void 0;
-const auth = (_req, _res, next) => {
-    console.log('authorizing user!');
-    next();
-};
-exports.auth = auth;
-//# sourceMappingURL=auth.js.map
